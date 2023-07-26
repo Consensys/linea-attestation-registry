9800bbf2dbd5ee8da9185878167fc1387102534e
Alain Nicolas <alain.nicolas@consensys.net>
2023-07-26 14:50:23 +0200

    feat: As a user, I can create a Schema

---
+++ test/SchemaRegistry.t.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import { Vm } from "forge-std/Vm.sol";
+import { Test } from "forge-std/Test.sol";
+import { SchemaRegistry } from "../src/SchemaRegistry.sol";
+
+contract SchemaRegistryTest is Test {
+  SchemaRegistry private schemaRegistry;
+  bytes32 private expectedId = 0x36304af55bbea73214675d3770f679b4b6e0bfff512f5af01046e6f5d29261e3;
+  string private expectedName = "Name";
+  string private expectedDescription = "Description";
+  string private expectedContext = "Context";
+  string private expectedString = "this is a schema";
+
+  event SchemaCreated(bytes32 indexed id, string name, string description, string context, string schemaString);
+  event Initialized(uint8 version);
+
+  function setUp() public {
+    schemaRegistry = new SchemaRegistry();
+  }
+
+  function testInitialize() public {
+    vm.expectEmit();
+    emit Initialized(1);
+    schemaRegistry.initialize();
+  }
+
+  function testGetIdFromSchemaString() public {
+    bytes32 id = schemaRegistry.getIdFromSchemaString(expectedString);
+    assertEq(id, expectedId);
+  }
+
+  function testCreateSchema() public {
+    vm.expectEmit();
+    emit SchemaCreated(expectedId, expectedName, expectedDescription, expectedContext, expectedString);
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, expectedString);
+
+    (string memory name, string memory description, string memory context, string memory schemaString) = schemaRegistry
+      .schemas(expectedId);
+    assertEq(name, expectedName);
+    assertEq(description, expectedDescription);
+    assertEq(context, expectedContext);
+    assertEq(schemaString, expectedString);
+  }
+
+  function testCannotCreateSchemaWithoutName() public {
+    vm.expectRevert(SchemaRegistry.SchemaNameMissing.selector);
+    schemaRegistry.createSchema("", expectedDescription, expectedContext, expectedString);
+  }
+
+  function testCannotCreateSchemaWithoutString() public {
+    vm.expectRevert(SchemaRegistry.SchemaStringMissing.selector);
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, "");
+  }
+
+  function testCannotCreateSchemaTwice() public {
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, expectedString);
+    vm.expectRevert(SchemaRegistry.SchemaAlreadyExists.selector);
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, expectedString);
+  }
+}

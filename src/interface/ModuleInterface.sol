7b0dc020b004430e902a6527156b05a1fe96d132
Satyajeet Kolhapure <77279246+satyajeetkolhapure@users.noreply.github.com>
2023-08-17 16:34:21 +0100

    feat: As a Verax Developer, the licence of the code produced is the MIT (#68)
    Co-authored-by: Satyajeet Kolhapure <kolhapure.satyajeet@gmail.com>
    Co-authored-by: Simon Brown <brown.j.simon@gmail.com>

---
+++ src/interface/ModuleInterface.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+//TODO: Replace this interface with proper module interface in future user story
+interface ModuleInterface {
+  function run() external view returns (bool);
+}

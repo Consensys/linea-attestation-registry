c0c13442776fcb80ba230e950b960ec2de3970a0
Alain Nicolas <alainncls@users.noreply.github.com>
2023-09-12 09:21:38 +0200

    chore: Improve contracts upgrade tooling (#165)

--- script/deploy/postDeployment.ts
+++ script/deploy/postDeployment.ts
@@ -1,23 +1,51 @@
 import { ethers } from "hardhat";
 import dotenv from "dotenv";
+import { Contract } from "ethers";
 
 dotenv.config({ path: "../.env" });
 
 async function main() {
   const proxyAddress = process.env.SCHEMA_REGISTRY_ADDRESS ?? "";
   console.log("Creating relationship schema using schema registry, with proxy at", proxyAddress);
-  const SchemaRegistry = await ethers.getContractFactory("SchemaRegistry");
-  const schemaRegistry = SchemaRegistry.attach(proxyAddress);
-  await createSchema(schemaRegistry, 
-      "Relationship", 
-      "Creates a named relationship between two attestations", 
-      "https://schema.org/Property", 
-      "bytes32 subject, string predicate, bytes32 object");
-  await createSchema(schemaRegistry, 
-      "namedGraphRelationship", 
-      "Creates a named relationship between two attestations inside a specific named graph", 
-      "https://schema.org/Property", 
-      "string namedGraph, bytes32 subject, string predicate, bytes32 object");
+  const schemaRegistry = await ethers.getContractAt("SchemaRegistry", proxyAddress);
+
+  await createSchema(
+    schemaRegistry,
+    "Relationship",
+    "Creates a named relationship between two attestations",
+    "https://schema.org/Property",
+    "bytes32 subject, string predicate, bytes32 object",
+  );
+  await createSchema(
+    schemaRegistry,
+    "namedGraphRelationship",
+    "Creates a named relationship between two attestations inside a specific named graph",
+    "https://schema.org/Property",
+    "string namedGraph, bytes32 subject, string predicate, bytes32 object",
+  );
+}
+
+async function createSchema(
+  schemaRegistry: Contract,
+  schemaName: string,
+  schemaDescription: string,
+  schemaContext: string,
+  schemaString: string,
+) {
+  const schemaId = await schemaRegistry.getIdFromSchemaString(schemaString);
+  const schemaExists = await schemaRegistry.isRegistered(schemaId);
+  if (!schemaExists) {
+    const tx = await schemaRegistry.createSchema(schemaName, schemaDescription, schemaContext, schemaString);
+    await tx.wait();
+    const schemaCreated = await schemaRegistry.isRegistered(schemaId);
+    if (schemaCreated) {
+      console.log(`Schema "${schemaName}" successfully created with ID ${schemaId}`);
+    } else {
+      throw new Error(`Schema "${schemaName}" creation failed!`);
+    }
+  } else {
+    throw new Error(`Schema "${schemaName}" already exists with ID ${schemaId}`);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -26,23 +54,3 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-
-async function createSchema(schemaRegistry, schemaName, schemaDescription, schemaContext, schemaString) {
-  const schemaId = await schemaRegistry.getIdFromSchemaString(schemaString);
-  const schemaExists = await schemaRegistry.isRegistered(schemaId);
-  if (!schemaExists) {
-    const tx = await schemaRegistry.createSchema(schemaName, schemaDescription, schemaContext, schemaString);
-    await tx.wait();
-    const schemaCreated = await schemaRegistry.isRegistered(schemaId);
-    if (schemaCreated)
-      console.log(`${schemaName} schema creation succeeded, schema id : ${schemaId}`);
-
-    else
-      console.log(`${schemaName} schema creation failed, schema id : ${schemaId}`);
-  }
-
-  else {
-    console.log(`${schemaName} schema already exists, schema id : ${schemaId}`);
-  }
-}
-

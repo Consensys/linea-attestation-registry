f817df0c816cef48454f6160f54d90a880082175
Alain Nicolas <alain.nicolas@consensys.net>
2023-09-14 16:11:17 +0200

    feat: As a user, I want the contracts data to be indexed via a subgraph

---
+++ subgraph/src/attestation-registry.ts
@@ -0,0 +1,26 @@
+import {
+  AttestationRegistered as AttestationRegisteredEvent,
+  AttestationRegistry,
+} from "../generated/AttestationRegistry/AttestationRegistry";
+import { Attestation } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export function handleAttestationRegistered(event: AttestationRegisteredEvent): void {
+  const contract = AttestationRegistry.bind(event.address);
+  const attestationData = contract.getAttestation(event.params.attestationId);
+  const attestation = new Attestation(event.params.attestationId.toHex());
+
+  attestation.schemaId = attestationData.schemaId;
+  attestation.replacedBy = attestationData.replacedBy;
+  attestation.attester = attestationData.attester;
+  attestation.portal = attestationData.portal;
+  attestation.attestedDate = attestationData.attestedDate;
+  attestation.expirationDate = attestationData.expirationDate;
+  attestation.revocationDate = attestationData.revocationDate;
+  attestation.version = BigInt.fromI32(attestationData.version);
+  attestation.revoked = attestationData.revoked;
+  attestation.subject = attestationData.subject;
+  attestation.attestationData = attestationData.attestationData;
+
+  attestation.save();
+}

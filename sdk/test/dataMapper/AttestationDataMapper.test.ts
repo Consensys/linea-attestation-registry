1be21808f4e41bfa1c1f0c943ee296f4664a7510
Satyajeet Kolhapure <77279246+satyajeetkolhapure@users.noreply.github.com>
2023-10-09 21:01:04 +0100

    chore: Add test framework (#261)
    Co-authored-by: Satyajeet Kolhapure <kolhapure.satyajeet@gmail.com>

---
+++ sdk/test/dataMapper/AttestationDataMapper.test.ts
@@ -0,0 +1,69 @@
+import AttestationDataMapper from "../../src/dataMapper/AttestationDataMapper";
+import VeraxSdk from "../../src/VeraxSdk";
+import { createPublicClient, PublicClient, http } from "viem";
+import { ApolloClient, InMemoryCache, ApolloQueryResult, gql } from "@apollo/client/core";
+//TODO : This is a basic test example. mock data and assertions should be more precise
+describe("AttestationDataMapper", () => {
+  let mockApolloClient: ApolloClient<object>;
+  let web3Client: PublicClient;
+  let attestationDataMapper: AttestationDataMapper;
+  const typeName: string = "attestation";
+  const gqlInterface: string = `{
+            id
+            schemaId
+            replacedBy
+            attester
+            portal
+            attestedDate
+            expirationDate
+            revocationDate
+            version
+            revoked
+            subject
+            attestationData
+            schemaString
+            decodedData
+  }`;
+
+  beforeAll(() => {
+    // Create web3Client
+    web3Client = createPublicClient({
+      chain: VeraxSdk.DEFAULT_LINEA_TESTNET.chain,
+      transport: http(),
+    });
+
+    // Create a mock Apollo Client with an in-memory cache
+    mockApolloClient = new ApolloClient({
+      cache: new InMemoryCache(),
+    });
+
+    attestationDataMapper = new AttestationDataMapper(VeraxSdk.DEFAULT_LINEA_TESTNET, web3Client, mockApolloClient);
+  });
+
+  afterEach(() => {
+    // Clear the mock function's call history after each test
+    jest.clearAllMocks();
+  });
+
+  describe("findOneById", () => {
+    it("should return the expected attestation", async () => {
+      const attestationId = "attestationId1";
+      // Mock the behavior of the query method
+      const queryMock = jest.spyOn(mockApolloClient, "query");
+      queryMock.mockResolvedValueOnce({
+        data: {
+          attestation: { result: "success" },
+        },
+      } as ApolloQueryResult<unknown>);
+
+      const result = await attestationDataMapper.findOneById(attestationId);
+
+      // Assert
+      expect(result).toBe('{"result":"success"}');
+      expect(mockApolloClient.query).toHaveBeenCalledWith({
+        query: gql(`query GetOne($id: ID!) { ${typeName}(id: $id) ${gqlInterface} }`),
+        variables: { id: attestationId },
+      });
+    });
+  });
+});

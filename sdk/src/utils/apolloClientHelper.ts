5bb3b67abfec1cb2932b2cd5b57c3b9be8cfa7b7
Satyajeet Kolhapure <kolhapure.satyajeet@gmail.com>
2023-10-10 16:39:44 +0100

    fix: improved stringifyWhereClause

--- sdk/src/utils/apolloClientHelper.ts
+++ sdk/src/utils/apolloClientHelper.ts
@@ -1,14 +1,4 @@
 export function stringifyWhereClause(whereClauseObj: Record<string, unknown>) {
-  let objLiteral = "{";
-  for (const key in whereClauseObj) {
-    if (Object.prototype.hasOwnProperty.call(whereClauseObj, key)) {
-      objLiteral += `${key}: "${whereClauseObj[key]}", `;
-    }
-  }
-  // Remove the trailing comma and space, if any
-  if (objLiteral.endsWith(", ")) {
-    objLiteral = objLiteral.slice(0, -2);
-  }
-  objLiteral += "}";
-  return objLiteral;
+  const json = JSON.stringify(whereClauseObj);
+  return json.replace(/"([^"]+)":/g, "$1:");
 }

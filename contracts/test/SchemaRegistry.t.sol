e04439e0091a91d78d8c0e8335e40349e44d9747
Satyajeet Kolhapure <77279246+satyajeetkolhapure@users.noreply.github.com>
2023-10-25 17:03:56 +0100

    fix: As an issuer, I can update another issuer's schema context (#319)
    Co-authored-by: Alain Nicolas <alain.nicolas@consensys.net>
    Co-authored-by: Satyajeet Kolhapure <kolhapure.satyajeet@gmail.com>

--- contracts/test/SchemaRegistry.t.sol
+++ contracts/test/SchemaRegistry.t.sol
@@ -17,6 +17,7 @@ contract SchemaRegistryTest is Test {
   string private expectedContext = "Context";
   string private expectedString = "this is a schema";
   address private user = makeAddr("user");
+  address private unassignedUser = makeAddr("unassignedUser");
 
   event SchemaCreated(bytes32 indexed id, string name, string description, string context, string schemaString);
   event Initialized(uint8 version);
@@ -32,6 +33,7 @@ contract SchemaRegistryTest is Test {
     portalRegistryAddress = address(portalRegistryMock);
     router.updatePortalRegistry(portalRegistryAddress);
     portalRegistryMock.setIssuer(user);
+    portalRegistryMock.setIssuer(unassignedUser);
   }
 
   function test_initialize_ContractAlreadyInitialized() public {
@@ -54,6 +56,42 @@ contract SchemaRegistryTest is Test {
     vm.expectRevert(SchemaRegistry.RouterInvalid.selector);
     vm.prank(address(0));
     testSchemaRegistry.updateRouter(address(0));
+  }
+
+  function test_updateSchemaIssuer() public {
+    vm.prank(user);
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, expectedString);
+    vm.prank(address(0));
+    schemaRegistry.updateSchemaIssuer(expectedId, address(2));
+  }
+
+  function test_updateSchemaIssuer_SchemaNotRegistered() public {
+    vm.expectRevert(SchemaRegistry.SchemaNotRegistered.selector);
+    vm.prank(address(0));
+    schemaRegistry.updateSchemaIssuer(expectedId, address(0));
+  }
+
+  function test_updateSchemaIssuer_IssuerInvalid() public {
+    vm.prank(user);
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, expectedString);
+    vm.prank(address(0));
+    vm.expectRevert(SchemaRegistry.IssuerInvalid.selector);
+    schemaRegistry.updateSchemaIssuer(expectedId, address(0));
+  }
+
+  function test_updateMatchingSchemaIssuers() public {
+    vm.prank(user);
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, expectedString);
+    vm.prank(portalRegistryAddress);
+    schemaRegistry.updateMatchingSchemaIssuers(user, address(2));
+  }
+
+  function test_updateMatchingSchemaIssuers_OnlyPortalRegistry() public {
+    vm.prank(user);
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, expectedString);
+    vm.prank(address(0));
+    vm.expectRevert(SchemaRegistry.OnlyPortalRegistry.selector);
+    schemaRegistry.updateMatchingSchemaIssuers(user, address(2));
   }
 
   function test_getIdFromSchemaString() public {
@@ -118,17 +156,19 @@ contract SchemaRegistryTest is Test {
     vm.stopPrank();
   }
 
-  function test_updateContext_OnlyIssuer() public {
-    vm.expectRevert(SchemaRegistry.OnlyIssuer.selector);
-    vm.startPrank(makeAddr("InvalidIssuer"));
-    schemaRegistry.updateContext(expectedId, "New context");
-    vm.stopPrank();
-  }
-
   function test_updateContext_SchemaNotRegistered() public {
     vm.startPrank(user);
     vm.expectRevert(SchemaRegistry.SchemaNotRegistered.selector);
     schemaRegistry.updateContext("Invalid ID", "New context");
+    vm.stopPrank();
+  }
+
+  function test_updateContext_OnlyAssignedIssuer() public {
+    vm.startPrank(user);
+    schemaRegistry.createSchema(expectedName, expectedDescription, expectedContext, expectedString);
+    vm.expectRevert(SchemaRegistry.OnlyAssignedIssuer.selector);
+    vm.startPrank(unassignedUser);
+    schemaRegistry.updateContext(expectedId, "New context");
     vm.stopPrank();
   }
 

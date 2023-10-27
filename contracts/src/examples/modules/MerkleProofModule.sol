88801232e6076f6443d285ab215d0b15a94cb524
Builddddder <147791546+Builddddder@users.noreply.github.com>
2023-10-27 20:23:19 +0800

    Feat: As a user, I want to have an example merkle proof module (#328)

---
+++ contracts/src/examples/modules/MerkleProofModule.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import { AbstractModule } from "../../interface/AbstractModule.sol";
+import { AttestationPayload } from "../../types/Structs.sol";
+
+contract MerkleProofModule is AbstractModule {
+  error MerkelProofVerifyFailed();
+
+  /**
+   * @inheritdoc AbstractModule
+   * @notice This method is used to run the module's validation logic
+   * @param attestationPayload - AttestationPayload containing the user address as `subject`
+   *                             and nonce as `attestationData`
+   * @param validationPayload - validationPayload containing the serialized hash.The last one is the 'Root'.
+   */
+  function run(
+    AttestationPayload memory attestationPayload,
+    bytes memory validationPayload,
+    address /*txSender*/,
+    uint256 /*value*/
+  ) public view override {
+    bytes32[] memory proof = abi.decode(validationPayload, (bytes32[]));
+    bytes32 hash = bytes32(attestationPayload.attestationData);
+    /*
+     * @notice We send the hardcoded third leaf to verify.
+     */
+    uint index = 2;
+    for (uint i = index + 1; i < proof.length; i++) {
+      bytes32 proofElement = proof[i];
+
+      if (index % 2 == 0) {
+        hash = keccak256(abi.encodePacked(hash, proofElement));
+      } else {
+        hash = keccak256(abi.encodePacked(proofElement, hash));
+      }
+      index = index / 2;
+      if (index == 0) {
+        break;
+      }
+    }
+    if (hash != proof[proof.length - 1]) {
+      revert MerkelProofVerifyFailed();
+    }
+  }
+}
---
+++ contracts/test/example/MerkleProofModule.t.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import { Test } from "forge-std/Test.sol";
+import { AbstractModule } from "../../src/interface/AbstractModule.sol";
+import { MerkleProofModule } from "../../src/examples/modules/MerkleProofModule.sol";
+import { AttestationPayload } from "../../src/types/Structs.sol";
+
+contract MerkleProofModuleTest is Test {
+  MerkleProofModule private merkleProofModule;
+
+  event ModuleRegistered(string name, string description, address moduleAddress);
+
+  function setUp() public {
+    merkleProofModule = new MerkleProofModule();
+  }
+
+  function buildMerkleTree() private returns (bytes32[] memory) {
+    bytes32[] memory hashes = new bytes32[](7);
+    string[4] memory transactions = ["alice -> bob", "bob -> dave", "carol -> alice", "dave -> bob"];
+    for (uint i = 0; i < transactions.length; i++) {
+      hashes[i] = keccak256(abi.encodePacked(transactions[i]));
+    }
+
+    uint n = transactions.length;
+    uint offset = 0;
+    uint current = n;
+
+    while (n > 0) {
+      for (uint i = 0; i < n - 1; i += 2) {
+        hashes[current] = keccak256(abi.encodePacked(hashes[offset + i], hashes[offset + i + 1]));
+        current += 1;
+      }
+      offset += n;
+      n = n / 2;
+    }
+    return hashes;
+  }
+
+  function test_MerkleProofModule_MerkleProofInvalidation() public {
+    bytes32[] memory hashes = buildMerkleTree();
+    // Replace by an invalid root node.
+    hashes[hashes.length - 1] = bytes32("0");
+    AttestationPayload memory attestationPayload = AttestationPayload(
+      bytes32("1234"),
+      0,
+      abi.encode("attestee_id"),
+      bytes.concat(hashes[2])
+    );
+    address user = makeAddr("user");
+    bytes memory proofData = abi.encode(hashes);
+    vm.expectRevert(MerkleProofModule.MerkelProofVerifyFailed.selector);
+    merkleProofModule.run(attestationPayload, proofData, user, 0);
+  }
+
+  function test_MerkleProofModule_MerkleProofVerifySuccess() public {
+    bytes32[] memory hashes = buildMerkleTree();
+    AttestationPayload memory attestationPayload = AttestationPayload(
+      bytes32("1234"),
+      0,
+      abi.encode("attestee_id"),
+      bytes.concat(hashes[2])
+    );
+    address user = makeAddr("user");
+    bytes memory proofData = abi.encode(hashes);
+    merkleProofModule.run(attestationPayload, proofData, user, 0);
+  }
+
+  function test_MerkleProofModule_supportsInterface() public {
+    bool isAbstractModuleSupported = merkleProofModule.supportsInterface(type(AbstractModule).interfaceId);
+    assertEq(isAbstractModuleSupported, true);
+  }
+}

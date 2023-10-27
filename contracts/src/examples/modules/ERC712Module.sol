aef98e4815e45e067028e0e353bfa3578de2cac0
Builddddder <builder@aspecta.id>
2023-10-28 01:16:10 +0800

    Add erc712 module

---
+++ contracts/src/examples/modules/ERC712Module.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import { AbstractModule } from "../../interface/AbstractModule.sol";
+import { AttestationPayload } from "../../types/Structs.sol";
+
+/**
+ * @notice Definition of EIP-712 domain
+ */
+struct EIP712Domain {
+  string name;
+  string version;
+  uint256 chainId;
+  address verifyingContract;
+}
+
+contract ERC712Module is AbstractModule {
+  EIP712Domain public domain;
+
+  error InvalidSignature();
+
+  constructor(EIP712Domain memory _domain) {
+    domain = _domain;
+  }
+
+  function hashDomain(EIP712Domain memory _domain) internal view returns (bytes32) {
+    return
+      keccak256(
+        abi.encode(
+          keccak256(bytes(_domain.name)),
+          keccak256(bytes(_domain.version)),
+          _domain.chainId,
+          _domain.verifyingContract
+        )
+      );
+  }
+
+  function run(
+    AttestationPayload memory attestationPayload,
+    bytes memory validationPayload,
+    address txSender,
+    uint256 /*value*/
+  ) public view override {
+    (uint8 v, bytes32 r, bytes32 s) = abi.decode(validationPayload, (uint8, bytes32, bytes32));
+
+    address signer = ecrecover(bytes32(attestationPayload.attestationData), v, r, s);
+    if (signer != txSender) {
+      revert InvalidSignature();
+    }
+  }
+}

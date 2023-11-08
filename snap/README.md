745f6940b3daafa6b25399c998bcd7b396f6fae0
Alain Nicolas <alainncls@users.noreply.github.com>
2023-11-08 07:47:34 +0100

    feat: As a user, I want to have a MetaMask Snap to check the attestations of the recipient of a transaction (#356)

---
+++ snap/README.md
@@ -0,0 +1,14 @@
+# Verax Checker Snap
+
+## Technical topics covered
+
+This project leverages:
+
+1. the [Verax Attestation Registry](https://verax.gitbook.io/verax/)
+2. the [MetaMask Snaps platform](https://docs.metamask.io/snaps/)
+
+## How to run
+
+```shell
+yarn install && yarn start
+```
---
+++ snap/packages/site/src/types/custom.d.ts
@@ -0,0 +1,18 @@
+/* eslint-disable*/
+
+/// <reference types="react-scripts" />
+
+import { MetaMaskInpageProvider } from "@metamask/providers";
+/*
+ * Window type extension to support ethereum
+ */
+
+declare global {
+  interface Window {
+    ethereum: MetaMaskInpageProvider & {
+      setProvider?: (provider: MetaMaskInpageProvider) => void;
+      detected?: MetaMaskInpageProvider[];
+      providers?: MetaMaskInpageProvider[];
+    };
+  }
+}

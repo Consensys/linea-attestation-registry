71fa6e2a6a32c4b87dd463d0b261fb193ceb1502
ollie <67156692+0xEillo@users.noreply.github.com>
2023-11-13 20:26:03 +0000

    feat: verax on arbitrum

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -20,6 +20,15 @@ const config: HardhatUserConfig = {
   defaultNetwork: "linea-goerli",
   networks: {
     hardhat: {},
+    "arbitrum-goerli": {
+      url: "https://goerli-rollup.arbitrum.io/rpc",
+      chainId: 421613,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    arbitrum: {
+      url: "https://arb1.arbitrum.io/rpc",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     "linea-goerli": {
       url: `https://linea-goerli.infura.io/v3/${process.env.INFURA_KEY ?? ""}`,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
@@ -42,12 +51,30 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
+      "arbitrum-goerli": process.env.ETHERSCAN_API_KEY ?? "KAFVN62ITWU9JZK5MZN9KC9DFX4VKM85BX",
+      arbitrum: process.env.ETHERSCAN_API_KEY ?? "KAFVN62ITWU9JZK5MZN9KC9DFX4VKM85BX",
       "linea-goerli": process.env.ETHERSCAN_API_KEY ?? "",
       linea: process.env.ETHERSCAN_API_KEY ?? "",
       sepolia: process.env.ETHERSCAN_API_KEY ?? "",
       "scroll-sepolia": process.env.SCROLL_API_KEY ?? "",
     },
     customChains: [
+      {
+        network: "arbitrum-goerli",
+        chainId: 421613,
+        urls: {
+          apiURL: "https://api-goerli.arbiscan.io/api",
+          browserURL: "https://goerli.arbiscan.io",
+        },
+      },
+      {
+        network: "arbitrum",
+        chainId: 42161,
+        urls: {
+          apiURL: "https://api.arbiscan.io/api",
+          browserURL: "https://arbiscan.io",
+        },
+      },
       {
         network: "linea-goerli",
         chainId: 59140,

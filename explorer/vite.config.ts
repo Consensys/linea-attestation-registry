4408eac912a441e4bc6c76352499fde2393eef90
Solniechniy <oleksndrkorn@gmail.com>
2023-11-20 16:48:36 +0200

    basic ui config

--- explorer/vite.config.ts
+++ explorer/vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

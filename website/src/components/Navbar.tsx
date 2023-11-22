5bf528ad9bfbbe6b96a59f4dcee561411384f899
Alain Nicolas <alainncls@users.noreply.github.com>
2023-11-22 18:14:43 +0100

    feat: As a user, I want to have a landing page (#408)

---
+++ website/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+            <NavLink className="navbar-brand" to="/" end>
+              Home
+            </NavLink></li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/poh" end>
+              Linea POH
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- website/src/pages/SDKDemo.tsx
+++ website/src/pages/SDKDemo.tsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import veraxLogo from "./assets/verax-logo-circle.svg";
-import "./App.css";
+import "./SDKDemo.css";
 import { ConnectKitButton } from "connectkit";
 import { VeraxSdk } from "@verax-attestation-registry/verax-sdk";
 
-function App() {
+function SDKDemo() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [attestations, setAttestations] = useState<any[]>([]);
   const [attestationsCounter, setAttestationsCounter] = useState<number>(0);
@@ -15,6 +14,7 @@ function App() {
     const veraxSdk = new VeraxSdk(VeraxSdk.DEFAULT_LINEA_TESTNET_FRONTEND);
     setAttestations(await veraxSdk.attestation.findBy(2));
   };
+
   const getAttestationCounter = async () => {
     const veraxSdk = new VeraxSdk(VeraxSdk.DEFAULT_LINEA_TESTNET_FRONTEND);
     setAttestationsCounter((await veraxSdk.utils.getAttestationIdCounter()) as number);
@@ -42,17 +42,14 @@ function App() {
     }
   };
 
+
   return (
     <>
-      <div>
-        <a href="https://docs.ver.ax/" target="_blank">
-          <img src={veraxLogo} className="logo" alt="Verax logo" />
-        </a>
-      </div>
-      <h1>Verax Attestation Registry</h1>
+      <h1>Verax - SDK Demo</h1>
       <div className="card" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
         <ConnectKitButton />
       </div>
+
       <div className="card">
         <button onClick={getSomeAttestations}>Get 2 attestations from the subgraph</button>
         {attestations.length > 0 && (
@@ -63,18 +60,19 @@ function App() {
           </ul>
         )}
       </div>
+
       <div className="card">
         <button onClick={getAttestationCounter}>Count attestations from the contract</button>
         {attestationsCounter > 0 && <p>{`We have ${attestationsCounter} attestations at the moment`}</p>}
       </div>
+
       <div className="card">
         <button onClick={createAnAttestation}>Create an attestation via a Portal</button>
         {txHash !== "" && <p>{`Transaction with hash ${txHash} sent!`}</p>}
         {error !== "" && <p style={{ color: "red" }}>{error}</p>}
       </div>
-      <p className="read-the-docs">Click on the Verax logo to learn more</p>
     </>
   );
 }
 
-export default App;
+export default SDKDemo;

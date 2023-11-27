184ab542ffebd0e1faeea142d8a7784b4c5f1063
ollie <67156692+0xEillo@users.noreply.github.com>
2023-11-27 15:38:03 +0000

    chore: add arbitrum SDK config

--- sdk/src/VeraxSdk.ts
+++ sdk/src/VeraxSdk.ts
@@ -1,4 +1,4 @@
-import { linea, lineaTestnet } from "viem/chains";
+import { arbitrum, arbitrumGoerli, linea, lineaTestnet } from "viem/chains";
 import AttestationDataMapper from "./dataMapper/AttestationDataMapper";
 import SchemaDataMapper from "./dataMapper/SchemaDataMapper";
 import ModuleDataMapper from "./dataMapper/ModuleDataMapper";
@@ -39,6 +39,36 @@ export class VeraxSdk {
 
   static DEFAULT_LINEA_TESTNET_FRONTEND: Conf = {
     ...VeraxSdk.DEFAULT_LINEA_TESTNET,
+    mode: SDKMode.FRONTEND,
+  };
+
+  static DEFAULT_ARBITRUM_TESTNET: Conf = {
+    chain: arbitrumGoerli,
+    mode: SDKMode.BACKEND,
+    subgraphUrl: "ADD_SUBGRAPH_URL_HERE",
+    portalRegistryAddress: "0x7d6a914C1e33C141CB4a5e0095c1075E5649aFB2",
+    moduleRegistryAddress: "0x58EE79284bE65b217Db408A0991314f9Ae84348A",
+    schemaRegistryAddress: "0x129043e80e0B4C7da61a622df0912c31D3414AA7",
+    attestationRegistryAddress: "0xCD839595FdA5A8111d5E03D42d9D9af60ee67B66",
+  };
+
+  static DEFAULT_ARBITRUM_TESTNET_FRONTEND: Conf = {
+    ...VeraxSdk.DEFAULT_ARBITRUM_TESTNET,
+    mode: SDKMode.FRONTEND,
+  };
+
+  static DEFAULT_ARBITRUM: Conf = {
+    chain: arbitrum,
+    mode: SDKMode.BACKEND,
+    subgraphUrl: "ADD_SUBGRAPH_URL_HERE",
+    portalRegistryAddress: "0x4042D0A54f997EE3a1b0F51e4813654199BFd8bD",
+    moduleRegistryAddress: "0x3acF4daAB6cbc01546Dd4a96c9665B398d48A4ba",
+    schemaRegistryAddress: "0xE96072F46EA0e42e538762dDc0aFa4ED8AE6Ec27",
+    attestationRegistryAddress: "0x335E9719e8eFE2a19A92E07BC4836160fC31cd7C",
+  };
+
+  static DEFAULT_ARBITRUM_FRONTEND: Conf = {
+    ...VeraxSdk.DEFAULT_ARBITRUM,
     mode: SDKMode.FRONTEND,
   };
 

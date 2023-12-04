8feb45953b07b0f02ebcf2081c68ed6a5c2e1260
Solniechniy <oleksndrkorn@gmail.com>
2023-12-04 13:51:08 +0200

    fix vite.config.ts

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD:explorer/vite.config.ts
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  plugins: [react(), tsconfigPaths({ projects: ['./explorer'] })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePluginRadar } from "vite-plugin-radar";
@@ -26,7 +12,6 @@ export default defineConfig({
       },
     }),
   ],
->>>>>>> a464ff0bb67bfecb1e815a72d6c269e50b559ffb:website/vite.config.ts
   optimizeDeps: {
     include: ['@verax-attestation-registry/verax-sdk'],
   },

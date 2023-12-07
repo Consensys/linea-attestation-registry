f2471ccb27467e8db102c602c2dd2e1c08a69bbb
Taras Oliynyk <taras.oliynyk@hapi.one>
2023-12-08 00:19:06 +0200

    feat: add schemas list

--- explorer/src/interfaces/swr/enum.ts
+++ explorer/src/interfaces/swr/enum.ts
@@ -1,5 +1,7 @@
 export enum SWRKeys {
   GET_ATTESTATION_BY_ID = "getAttestationByID",
+  GET_SCHEMAS_LIST = "getSchemasList",
+  GET_SCHEMAS_COUNT = "getSchemasCount",
   GET_SCHEMA_BY_ID = "getSchemaByID",
   GET_RELATED_ATTESTATION = "getRelatedAttestations",
   GET_ATTESTATION_LIST = "getAttestationList",

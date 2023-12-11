9f8bb09367868a7b46897eb7fd8e299bcd762af8
voenkomatiwe <radionpon21@gmail.com>
2023-12-11 11:34:47 +0200

    fix: helper indocator

--- explorer/src/components/HelperIndicator/index.tsx
+++ explorer/src/components/HelperIndicator/index.tsx
@@ -1,5 +1,21 @@
 import { Page } from "@/interfaces/components";
 
+const getIndicatorColorClass = (page: Page): string => {
+  switch (page) {
+    case "schema":
+      return "bg-page-schema";
+    case "attestation":
+      return "bg-page-attestation";
+    case "portal":
+      return "bg-page-portal";
+    case "module":
+      return "bg-page-module";
+    default:
+      return "bg-transparent";
+  }
+};
+
 export const HelperIndicator: React.FC<{ type: Page }> = ({ type }) => {
-  return <div className={`w-[3.33px] h-[13.33px] bg-page-${type} rounded-full`} />;
+  const indicatorColor = getIndicatorColorClass(type);
+  return <div className={`w-[3.33px] h-[13.33px] ${indicatorColor} rounded-full`} />;
 };

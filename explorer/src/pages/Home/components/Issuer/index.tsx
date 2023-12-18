92985b8d70b8340a799fa807d1212d4e64b262ba
Taras Oliynyk <taras.oliynyk@hapi.one>
2023-12-18 18:00:33 +0200

    fix: main page ui fixes

--- explorer/src/pages/Home/components/Issuer/index.tsx
+++ explorer/src/pages/Home/components/Issuer/index.tsx
@@ -1,5 +1,9 @@
+import { t } from "i18next";
+import { ArrowUpRight, ChevronRight } from "lucide-react";
 import { useLocation, useNavigate } from "react-router-dom";
 
+import { Button } from "@/components/Buttons";
+import { EButtonType } from "@/components/Buttons/enum";
 import { Chips } from "@/components/Chips";
 import { useNetworkContext } from "@/providers/network-provider/context";
 import { CHAIN_ID_ROUTE, toIssuerById } from "@/routes/constants";
@@ -18,10 +22,7 @@ export const Issuer: React.FC<IIssuerProps> = ({ issuer }) => {
   return (
     <div
       key={issuer.name}
-      className="group flex flex-col gap-4 border border-border-card rounded-xl p-4 md:p-6 cursor-pointer hover:bg-surface-secondary"
-      onClick={() =>
-        navigate(toIssuerById(issuer.address).replace(CHAIN_ID_ROUTE, network), { state: { from: location.pathname } })
-      }
+      className="group flex flex-col gap-4 border border-border-card rounded-xl p-4 md:p-6 hover:bg-surface-secondary transition md:min-h-[27.8125rem]"
     >
       <div className="flex items-center gap-3 text-xl md:text-2xl font-semibold text-blackDefault">
         <div className="w-[2.5rem] h-[2.5rem] md:w-[3rem] md:h-[3rem] flex items-center justify-center">
@@ -46,6 +47,28 @@ export const Issuer: React.FC<IIssuerProps> = ({ issuer }) => {
           <div className="text-xs font-normal text-text-darkGrey">Schemas</div>
         </div>
       </div> */}
+      <div className="flex flex-1 flex-col lg:flex-row lg:items-end gap-4 justify-end lg:justify-start">
+        <Button
+          isSmall
+          name={t("common.actions.details")}
+          handler={() =>
+            navigate(toIssuerById(issuer.address).replace(CHAIN_ID_ROUTE, network), {
+              state: { from: location.pathname },
+            })
+          }
+          buttonType={EButtonType.OUTLINED}
+          iconRight={<ChevronRight />}
+        />
+        {issuer.CTALink && (
+          <Button
+            isSmall
+            name={issuer.CTATitle}
+            handler={() => window.open(issuer.CTALink, "_blank")}
+            buttonType={EButtonType.TRANSPARENT}
+            iconRight={<ArrowUpRight />}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- explorer/src/pages/Home/components/TitleSearch/index.tsx
+++ explorer/src/pages/Home/components/TitleSearch/index.tsx
@@ -7,22 +7,15 @@ import { EButtonType } from "@/components/Buttons/enum";
 import { EMPTY_STRING } from "@/constants";
 import { keyboard } from "@/constants/keyboard";
 import { useHandleSearch } from "@/hooks/useHandleSearch";
-import useWindowDimensions from "@/hooks/useWindowDimensions";
 
 export const TitleSearch: React.FC = () => {
-  const { sm } = useWindowDimensions();
-
   const [searchQuery, setSearchQuery] = useState<string>(EMPTY_STRING);
   const handleSearch = useHandleSearch();
 
   return (
-    <div className="flex flex-col items-center justify-center h-[24.125rem] lg:h-[23rem] md:h-[22.375rem] bg-surface-lime rounded-[1.375rem] lg:rounded-[2.25rem] md:rounded-[2rem] mt-5 md:mt-6 gap-10 px-4 md:px-10">
-      <h1 className="text-center font-medium md:font-semibold text-[2rem] md:text-[2.5rem] lg:text-[2.75rem] lg:leading-[2.97rem] md:leading-[2.7rem] leading-[2.25rem]">
-        <div>
-          {t("home.title")}
-          {!sm && ":"}
-        </div>
-        {!sm && <div>{t("home.subtitle")}</div>}
+    <div className="flex flex-col items-center justify-center h-[24.125rem] lg:h-[23rem] md:h-[22.375rem] bg-jumbotron rounded-[1.375rem] lg:rounded-[2.25rem] md:rounded-[2rem] mt-5 md:mt-6 gap-10 px-4 md:px-10">
+      <h1 className="text-center font-medium md:font-semibold text-[2rem] md:text-[2.5rem] lg:text-[2.75rem] lg:leading-[2.97rem] md:leading-[2.7rem] leading-[2.25rem] max-w-[52.5rem]">
+        {t("home.title")}
       </h1>
       <div className="flex flex-col md:flex-row w-full gap-2 max-w-[57.6875rem]">
         <input

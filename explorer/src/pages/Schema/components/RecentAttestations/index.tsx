c6b1189e9b9d6fac3618acf3a2c66992d42b05db
Taras Oliynyk <taras.oliynyk@hapi.one>
2023-12-20 14:22:24 +0200

    fix: make portal and schema columns visible in schema's related attestations table

--- explorer/src/pages/Schema/components/RecentAttestations/index.tsx
+++ explorer/src/pages/Schema/components/RecentAttestations/index.tsx
@@ -25,7 +25,7 @@ export const RecentAttestations: React.FC<{ schemaId: string }> = ({ schemaId })
   const columnsSkeletonRef = useRef(columnsSkeleton(columns({ sortByDate: false }), attestationColumnsOption));
   const data = isLoading
     ? { columns: columnsSkeletonRef.current, list: skeletonAttestations(5) }
-    : { columns: columns({ sortByDate: false }), list: attestations?.slice(-5).reverse() || [] };
+    : { columns: columns({ sortByDate: false, sdk }), list: attestations?.slice(-5).reverse() || [] };
 
   return (
     <div className="flex flex-col gap-6 w-full px-5 md:px-10">

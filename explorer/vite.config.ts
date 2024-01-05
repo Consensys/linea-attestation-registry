e4ecab5925824794c93cecbe17d2d5e65d4f1d34
Alain Nicolas <alainncls@users.noreply.github.com>
2024-01-05 09:04:01 +0100

    feat: Add a Google Analytics tag to the explorer (#501)

--- explorer/vite.config.ts
+++ explorer/vite.config.ts
@@ -2,11 +2,21 @@ import path from "path";
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePluginRadar } from "vite-plugin-radar";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths({ projects: ["./explorer"] }), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths({ projects: ["./explorer"] }),
+    svgr(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-SDPCWGQYK3",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

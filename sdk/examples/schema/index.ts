defc07ed9bd16121ca3b7012e9086dc89d02dbc8
Satyajeet Kolhapure <kolhapure.satyajeet@gmail.com>
2024-01-11 11:20:36 +0000

    fix: updated examples to load private key from env file

--- sdk/examples/schema/index.ts
+++ sdk/examples/schema/index.ts
@@ -1,13 +1,22 @@
+import { Hex } from "viem";
 import { VeraxSdk } from "../../src/VeraxSdk";
 import PortalExamples from "./schemaExamples";
+import { config } from "dotenv";
+import * as path from "path";
+
+const envPath = path.resolve(__dirname, "../../.env");
+config({ path: envPath });
+
+const privateKey = process.env.PRIVATE_KEY as Hex;
 
 let argv: string | null | undefined = process.argv[3] as string;
-argv !== null && argv !== undefined ? (argv = argv.replaceAll("\\", "")) : (argv = "");
+argv !== null && argv !== undefined && argv !== "wait" ? (argv = argv.replaceAll("\\", "")) : (argv = "");
 
 const waitForConfirmationArgv: string | null | undefined = process.argv[4] as string;
 let waitForConfirmation = false;
-if (waitForConfirmationArgv === "wait") waitForConfirmation = true;
+if ((argv === "" && (process.argv[3] as string) === "wait") || waitForConfirmationArgv === "wait")
+  waitForConfirmation = true;
 
-const veraxSdk = new VeraxSdk(VeraxSdk.DEFAULT_LINEA_TESTNET);
+const veraxSdk = new VeraxSdk(VeraxSdk.DEFAULT_LINEA_TESTNET, undefined, privateKey);
 
 new PortalExamples(veraxSdk).run(argv, process.argv[2], waitForConfirmation);

91a9d80341daffac8def4d7ab0c4392a34ee5c00
Alain Nicolas <alainncls@users.noreply.github.com>
2024-06-06 21:31:11 +0200

    fix(explorer): Explorer is not displaying all the attestations from a given address (#620)

--- explorer/src/pages/Search/components/SearchAttestationsReceived/loadAttestationReceivedList.ts
+++ explorer/src/pages/Search/components/SearchAttestationsReceived/loadAttestationReceivedList.ts
@@ -1,24 +1,20 @@
 import AttestationDataMapper from "@verax-attestation-registry/verax-sdk/lib/types/src/dataMapper/AttestationDataMapper";
 
-import { ITEMS_PER_PAGE_DEFAULT } from "@/constants";
+import { ITEMS_SEARCHED_DEFAULT } from "@/constants";
 import { ResultParseSearch } from "@/interfaces/components";
 import { isNotNullOrUndefined } from "@/utils";
 import { uniqMap } from "@/utils/searchUtils";
 
-export const loadAttestationList = async (
+export const loadAttestationReceivedList = async (
   attestation: AttestationDataMapper,
   parsedString: Partial<ResultParseSearch>,
 ) => {
-  const [listByAddress] = parsedString.address
+  const [listBySubject] = parsedString.address
     ? await Promise.all(
         parsedString.address.map(async (address) => {
-          const attestationResults = attestation.findBy(ITEMS_PER_PAGE_DEFAULT, undefined, {
-            attester_contains: address,
+          return attestation.findBy(ITEMS_SEARCHED_DEFAULT, undefined, {
+            subject: address,
           });
-          const subjectResults = attestation.findBy(ITEMS_PER_PAGE_DEFAULT, undefined, {
-            subject_contains: address,
-          });
-          return Promise.all([attestationResults, subjectResults]);
         }),
       )
     : [];
@@ -30,15 +26,10 @@ export const loadAttestationList = async (
   ).filter(isNotNullOrUndefined);
 
   const listBySchemaString = parsedString.schemaString
-    ? await attestation.findBy(ITEMS_PER_PAGE_DEFAULT, undefined, { schemaString: parsedString.schemaString })
+    ? await attestation.findBy(ITEMS_SEARCHED_DEFAULT, undefined, { schemaString: parsedString.schemaString })
     : [];
 
-  const results = [
-    ...(listByIds || []),
-    ...(listByAddress?.[0] || []),
-    ...(listByAddress?.[1] || []),
-    ...(listBySchemaString || []),
-  ];
+  const results = [...(listByIds || []), ...(listBySubject || []), ...(listBySchemaString || [])];
 
   return uniqMap(results, "id");
 };
--- explorer/src/pages/Search/index.tsx
+++ explorer/src/pages/Search/index.tsx
@@ -1,8 +1,9 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Trans } from "react-i18next";
 import { useSearchParams } from "react-router-dom";
 
 import archive from "@/assets/icons/archive.svg";
+import magnifyingGlass from "@/assets/icons/magnifying-glass.svg";
 import { InfoBlock } from "@/components/InfoBlock";
 import { EMPTY_STRING } from "@/constants";
 import { DEFAULT_SEARCH_ELEMENTS } from "@/constants/components";
@@ -10,7 +11,7 @@ import { EQueryParams } from "@/enums/queryParams";
 import { Page, SearchElementProps } from "@/interfaces/components";
 import { parseSearch } from "@/utils/searchUtils";
 
-import { SearchAttestations } from "./components/SearchAttestations";
+import { SearchAttestationsReceived } from "./components/SearchAttestationsReceived";
 import { SearchModules } from "./components/SearchModules";
 import { SearchPortals } from "./components/SearchPortals";
 import { SearchSchemas } from "./components/SearchSchemas";
@@ -22,17 +23,37 @@ export const Search = () => {
   const parsedString = useMemo(() => parseSearch(search), [search]);
 
   const [searchElements, setSearchElements] = useState<SearchElementProps>(DEFAULT_SEARCH_ELEMENTS);
-  const count = Object.values(searchElements).reduce((acc, currentValue) => acc + currentValue.count, 0);
-  const isLoaded = Object.values(searchElements).every((element) => element.loaded);
+  const [notFound, setNotFound] = useState(false);
+  const [count, setCount] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const newCount = Object.values(searchElements).reduce((acc, currentValue) => acc + currentValue.count, 0);
+    const newIsLoaded = Object.values(searchElements).every((element) => element.loaded);
+
+    setCount(newCount);
+    setIsLoaded(newIsLoaded);
+    setNotFound(newIsLoaded && newCount === 0);
+  }, [searchElements]);
 
   const updateSearchElement = (page: Page, count: number, loaded: boolean) => {
     setSearchElements((prev) => ({ ...prev, [page]: { loaded, count } }));
   };
-  const notFound = isLoaded && count === 0;
 
   return (
     <section className="container flex flex-col items-start gap-10 lg:mt-16 md:mt-8 mt-5">
-      {notFound ? (
+      {!isLoaded && (
+        <InfoBlock
+          icon={<img src={magnifyingGlass} alt="magnifying glass" />}
+          message={
+            <span className="break-all [&>strong]:text-text-primary dark:[&>strong]:text-text-secondaryDark">
+              <Trans i18nKey="common.messages.searchInProgress" values={{ search }} components={{ bold: <strong /> }} />
+            </span>
+          }
+        />
+      )}
+
+      {notFound && (
         <InfoBlock
           icon={<img src={archive} alt="archive" />}
           message={
@@ -41,35 +62,36 @@ export const Search = () => {
             </span>
           }
         />
-      ) : (
-        <>
-          <p className="text-text-tertiary text-base dark:text-tertiary [&>strong]:text-text-primary dark:[&>strong]:text-text-secondaryDark break-word">
-            <Trans i18nKey="common.messages.searchFound" values={{ count, search }} components={{ bold: <strong /> }} />
-          </p>
-          <div className="flex flex-col gap-12 w-full">
-            <SearchAttestations
-              search={search || EMPTY_STRING}
-              parsedString={parsedString}
-              getSearchData={(count, loaded) => updateSearchElement("attestation", count, loaded)}
-            />
-            <SearchSchemas
-              search={search || EMPTY_STRING}
-              parsedString={parsedString}
-              getSearchData={(count, loaded) => updateSearchElement("schema", count, loaded)}
-            />
-            <SearchModules
-              search={search || EMPTY_STRING}
-              parsedString={parsedString}
-              getSearchData={(count, loaded) => updateSearchElement("module", count, loaded)}
-            />
-            <SearchPortals
-              search={search || EMPTY_STRING}
-              parsedString={parsedString}
-              getSearchData={(count, loaded) => updateSearchElement("portal", count, loaded)}
-            />
-          </div>
-        </>
       )}
+
+      {count > 0 && (
+        <p className="text-text-tertiary text-base dark:text-tertiary [&>strong]:text-text-primary dark:[&>strong]:text-text-secondaryDark break-word">
+          <Trans i18nKey="common.messages.searchFound" values={{ count, search }} components={{ bold: <strong /> }} />
+        </p>
+      )}
+
+      <div className="flex flex-col gap-12 w-full">
+        <SearchAttestationsReceived
+          search={search || EMPTY_STRING}
+          parsedString={parsedString}
+          getSearchData={(count, loaded) => updateSearchElement("attestation", count, loaded)}
+        />
+        <SearchSchemas
+          search={search || EMPTY_STRING}
+          parsedString={parsedString}
+          getSearchData={(count, loaded) => updateSearchElement("schema", count, loaded)}
+        />
+        <SearchModules
+          search={search || EMPTY_STRING}
+          parsedString={parsedString}
+          getSearchData={(count, loaded) => updateSearchElement("module", count, loaded)}
+        />
+        <SearchPortals
+          search={search || EMPTY_STRING}
+          parsedString={parsedString}
+          getSearchData={(count, loaded) => updateSearchElement("portal", count, loaded)}
+        />
+      </div>
     </section>
   );
 };

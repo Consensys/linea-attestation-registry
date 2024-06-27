830bb8da89755233622473096b473418d14f52b6
Satyajeet Kolhapure <satyajeet.kolhapure@consensys.net>
2024-06-27 14:57:04 +0100

    updated utils examples with sepolia as default network

--- sdk/examples/utils/getSchemasNumber.ts
+++ sdk/examples/utils/getSchemasNumber.ts
@@ -1,5 +1,5 @@
 import { VeraxSdk } from "../../src/VeraxSdk";
 
-const veraxSdk = new VeraxSdk(VeraxSdk.DEFAULT_LINEA_TESTNET);
+const veraxSdk = new VeraxSdk(VeraxSdk.DEFAULT_LINEA_SEPOLIA);
 
 veraxSdk.utils.getSchemasNumber().then((res) => console.log(res));

af94f913dc9133366b20a3dc9f772252ff6f5edc
Satyajeet Kolhapure <satyajeet.kolhapure@consensys.net>
2024-08-23 09:58:17 +0100

    fixed sort order on schema page for attestations table

--- explorer/src/pages/Schema/components/RecentAttestations/index.tsx
+++ explorer/src/pages/Schema/components/RecentAttestations/index.tsx
@@ -17,7 +17,7 @@ export const RecentAttestations: React.FC<{ schemaId: string }> = ({ schemaId })
 
   const { data: attestations, isLoading } = useSWR(
     `${SWRKeys.GET_RECENT_ATTESTATION}/${schemaId}/${chain.id}`,
-    () => sdk.attestation.findBy(5, 0, { schemaId }, "attestedDate", "desc"),
+    () => sdk.attestation.findBy(5, undefined, { schemaId }, "attestedDate", "asc"),
     {
       shouldRetryOnError: false,
     },

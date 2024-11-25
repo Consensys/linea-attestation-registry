c56c71b93e3b66eec5a9adc0cb05cbc082615537
Alain Nicolas <alainncls@users.noreply.github.com>
2024-11-25 17:40:02 +0100

    feat(explorer): As a user, I want the Subject Page title to be explicit (#816)

--- explorer/src/pages/Subject/index.tsx
+++ explorer/src/pages/Subject/index.tsx
@@ -1,10 +1,18 @@
 import { OrderDirection } from "@verax-attestation-registry/verax-sdk/lib/types/.graphclient";
+import { Check, Copy } from "lucide-react";
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useParams } from "react-router-dom";
 import useSWR from "swr";
+import { isAddress } from "viem";
 
+import { Title } from "@/components/Title";
+import { THOUSAND } from "@/constants";
 import { EQueryParams } from "@/enums/queryParams";
+import useWindowDimensions from "@/hooks/useWindowDimensions.ts";
 import { SWRKeys } from "@/interfaces/swr/enum";
 import { useNetworkContext } from "@/providers/network-provider/context";
+import { cropString } from "@/utils/stringUtils.ts";
 
 import { CardView } from "../Attestations/components/CardView";
 
@@ -16,6 +24,20 @@ export const Subject: React.FC = () => {
   } = useNetworkContext();
   const searchParams = new URLSearchParams(window.location.search);
   const sortByDateDirection = searchParams.get(EQueryParams.SORT_BY_DATE);
+
+  const { sm } = useWindowDimensions();
+
+  const [copied, setCopied] = useState<boolean>(false);
+
+  const handleCopy = (text: string, result: boolean) => {
+    if (!result || !text) return;
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, THOUSAND);
+  };
+
+  const CopyIcon = copied ? Check : Copy;
 
   const { data: attestationsList } = useSWR(
     `${SWRKeys.GET_ATTESTATION_LIST}/${subject}/${sortByDateDirection}/${chain.id}`,
@@ -29,5 +51,20 @@ export const Subject: React.FC = () => {
       ),
   );
 
-  return attestationsList && <CardView attestationsList={attestationsList}></CardView>;
+  return (
+    <div className="container mt-5 md:mt-8">
+      <Title title={"Explore Attestations"} />
+      <div className="flex items-center text-text-secondary dark:text-text-secondaryDark text-base font-medium gap-3 mb-6">
+        <div>With subject {sm && subject && isAddress(subject) ? cropString(subject) : subject}</div>
+        <CopyToClipboard onCopy={handleCopy} text={subject ?? ""}>
+          <CopyIcon
+            className={`w-4 cursor-pointer hover:opacity-60 transition ${
+              copied ? "text-greenDefault dark:text-greenDark" : "text-greyDefault dark:text-text-secondaryDark"
+            } `}
+          />
+        </CopyToClipboard>
+      </div>
+      {attestationsList && <CardView attestationsList={attestationsList}></CardView>}
+    </div>
+  );
 };

94fd13e61e291d3e66151eb68ec8e7c63cf468a6
Satyajeet Kolhapure <satyajeet.kolhapure@consensys.net>
2024-11-26 11:22:49 +0000

    made getAttester funciton internal

--- contracts/src/interfaces/IPortal.sol
+++ contracts/src/interfaces/IPortal.sol
@@ -16,10 +16,4 @@ interface IPortal is IERC165 {
    * @return The list of modules addresses linked to the Portal
    */
   function getModules() external view returns (address[] memory);
-
-  /**
-   * @notice Defines the address of the entity issuing attestations to the subject
-   * @dev We strongly encourage a reflection when implementing this method
-   */
-  function getAttester() external view returns (address);
 }

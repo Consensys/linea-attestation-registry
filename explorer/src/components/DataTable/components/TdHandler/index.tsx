3425718ad3014bcee8670286b0675836b0126235
Chirag <ckotian117@gmail.com>
2024-12-07 10:09:06 +0530

    Refactor TdHandler and Tooltip components with minor styling adjustments

--- explorer/src/components/DataTable/components/TdHandler/index.tsx
+++ explorer/src/components/DataTable/components/TdHandler/index.tsx
@@ -6,29 +6,29 @@ import { ITdHandler } from "./interface";
 
 export const TdHandler: React.FC<ITdHandler> = ({ value, valueUrl, to, isTextLeft = false }) => {
   return (
-    <div className="flex items-center justify-end gap-2">
+    <div className="flex items-center justify-end gap-1 min-w-[120px]">
       {valueUrl ? (
         <a
           href={valueUrl}
           target="_blank"
           onClick={(e) => e.stopPropagation()}
-          className={`hover:underline ${isTextLeft ? "translate-x-0" : "translate-x-5"} ${
-            isTextLeft ? "group-hover:translate-x-5" : "group-hover:translate-x-0"
+          className={`hover:underline ${isTextLeft ? "translate-x-0" : "translate-x-2"} ${
+            isTextLeft ? "group-hover:translate-x-2" : "group-hover:translate-x-0"
           } transition max-w-[300px] overflow-hidden text-ellipsis`}
         >
           {value}
         </a>
       ) : (
         <p
-          className={`w-full text-${isTextLeft ? "left" : "right"} ${isTextLeft ? "translate-x-0" : "translate-x-5"} ${
-            isTextLeft ? "group-hover:-translate-x-5" : "group-hover:translate-x-0"
+          className={`w-full text-${isTextLeft ? "left" : "right"} ${isTextLeft ? "translate-x-0" : "translate-x-2"} ${
+            isTextLeft ? "group-hover:-translate-x-2" : "group-hover:translate-x-0"
           } transition max-w-[300px] overflow-hidden text-ellipsis`}
         >
           {value}
         </p>
       )}
       <Link to={to} onClick={(e) => e.stopPropagation()}>
-        <ChevronRight className="w-5 opacity-0 group-hover:opacity-100 transition" />
+        <ChevronRight className="w-4 opacity-0 group-hover:opacity-100 transition" />
       </Link>
     </div>
   );

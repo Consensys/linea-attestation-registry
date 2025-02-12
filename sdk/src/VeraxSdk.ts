174c736570c3b861015c366414b55b480db82dfc
Chirag-S-Kotian <ckotian117@gmail.com>
2025-02-12 18:00:50 +0530

    refactor(VeraxSdk): remove unused rpcUrl property from configuration objects

--- sdk/src/VeraxSdk.ts
+++ sdk/src/VeraxSdk.ts
@@ -32,7 +32,6 @@ export class VeraxSdk {
     moduleRegistryAddress: "0xf851513A732996F22542226341748f3C9978438f",
     schemaRegistryAddress: "0x0f95dCec4c7a93F2637eb13b655F2223ea036B59",
     attestationRegistryAddress: "0x3de3893aa4Cdea029e84e75223a152FD08315138",
-    rpcUrl: undefined, // Added rpcUrl
   };
 
   static DEFAULT_LINEA_MAINNET_FRONTEND: Conf = {
@@ -48,7 +47,6 @@ export class VeraxSdk {
     moduleRegistryAddress: "0x3C443B9f0c8ed3A3270De7A4815487BA3223C2Fa",
     schemaRegistryAddress: "0x90b8542d7288a83EC887229A7C727989C3b56209",
     attestationRegistryAddress: "0xDaf3C3632327343f7df0Baad2dc9144fa4e1001F",
-    rpcUrl: undefined,
   };
 
   static DEFAULT_LINEA_SEPOLIA_FRONTEND: Conf = {
@@ -64,7 +62,6 @@ export class VeraxSdk {
     moduleRegistryAddress: "0xEC572277d4E87a64DcfA774ED219Dd4E69E4BDc6",
     schemaRegistryAddress: "0x025531b655D9EE335B8E6cc4C118b313f26ACc8F",
     attestationRegistryAddress: "0xee5e23492bf49C1F4CF0676b3bF49d78A6dD61c5",
-    rpcUrl: undefined,
   };
 
   static DEFAULT_ARBITRUM_SEPOLIA_FRONTEND: Conf = {
@@ -80,7 +77,6 @@ export class VeraxSdk {
     moduleRegistryAddress: "0x3acF4daAB6cbc01546Dd4a96c9665B398d48A4ba",
     schemaRegistryAddress: "0xE96072F46EA0e42e538762dDc0aFa4ED8AE6Ec27",
     attestationRegistryAddress: "0x335E9719e8eFE2a19A92E07BC4836160fC31cd7C",
-    rpcUrl: undefined,
   };
 
   static DEFAULT_ARBITRUM_FRONTEND: Conf = {
@@ -96,7 +92,6 @@ export class VeraxSdk {
     moduleRegistryAddress: "0xEC572277d4E87a64DcfA774ED219Dd4E69E4BDc6",
     schemaRegistryAddress: "0x66D2F3DCc970343b83a6263E20832184fa71CFe7",
     attestationRegistryAddress: "0x374B686137eC0DB442a8d833451f8C12cD4B5De4",
-    rpcUrl: undefined,
   };
 
   static DEFAULT_BASE_SEPOLIA_FRONTEND: Conf = {
@@ -112,7 +107,6 @@ export class VeraxSdk {
     moduleRegistryAddress: "0xAd0C12db58098A6665CBEf48f60eB67d81d1F1ff",
     schemaRegistryAddress: "0x8081dCd745f160c148Eb5be510F78628A0951c31",
     attestationRegistryAddress: "0xA0080DBd35711faD39258E45d9A5D798852b05D4",
-    rpcUrl: undefined,
   };
 
   static DEFAULT_BASE_FRONTEND: Conf = {
@@ -128,7 +122,6 @@ export class VeraxSdk {
     moduleRegistryAddress: "0x6c46c245918d4fcfC13F0a9e2e49d4E2739A353a",
     schemaRegistryAddress: "0x51929da151eC2C5a5881C750E5b9941eACC46c1d",
     attestationRegistryAddress: "0x5Cc4029f0dDae1FFE527385459D06d81DFD50EEe",
-    rpcUrl: undefined,
   };
 
   static DEFAULT_BSC_TESTNET_FRONTEND: Conf = {
@@ -144,7 +137,6 @@ export class VeraxSdk {
     moduleRegistryAddress: "0xD70a06f7A0f197D55Fa841fcF668782b2B8266eB",
     schemaRegistryAddress: "0x29205492435E1b06B20CeAeEC4AC41bcF595DFFd",
     attestationRegistryAddress: "0x3D8A3a8FF21bD295dbBD5319C399e2C4FD27F261",
-    rpcUrl: undefined,
   };
 
   static DEFAULT_BSC_FRONTEND: Conf = {
@@ -162,18 +154,25 @@ export class VeraxSdk {
   public utils: UtilsDataMapper;
 
   constructor(conf: Conf, publicAddress?: Address, privateKey?: Hex) {
-    const transport: Transport = conf.rpcUrl ? http(conf.rpcUrl) : http();
+    const transport: Transport =
+      conf.mode === SDKMode.FRONTEND
+        ? conf.rpcUrl
+          ? http(conf.rpcUrl)
+          : custom(window.ethereum)
+        : conf.rpcUrl
+        ? http(conf.rpcUrl)
+        : http();
 
     this.web3Client = createPublicClient({
       chain: conf.chain,
-      transport: transport,
+      transport,
     });
 
     if (conf.mode === SDKMode.BACKEND) {
       this.walletClient = createWalletClient({
         chain: conf.chain,
         account: privateKey ? privateKeyToAccount(privateKey) : undefined,
-        transport: transport,
+        transport,
       });
     } else if (typeof window.ethereum !== "undefined") {
       this.walletClient = createWalletClient({
